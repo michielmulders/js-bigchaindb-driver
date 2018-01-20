fca138a5a9a35f70439af7989fdb17c30872ae58
Michiel Mulders <michielmulders1@gmail.com>
2018-01-21 00:17:01 +0100

    top sort still no working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export Ed25519Keypair from './Ed25519Keypair'
 
 export Connection from './connection'
 export Transaction from './transaction'
+export topsortTransactions from './utils/topologySorting'
 export ccJsonLoad from './utils/ccJsonLoad'
 export ccJsonify from './utils/ccJsonify'
-export topsortTransactions from './utils/topologySorting'
+export topsort from './utils/topologySorting'
