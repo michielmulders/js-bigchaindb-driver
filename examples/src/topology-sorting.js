fb89545bd9b7982ab9bc2ce82a6f2a047373f48d
Michiel Mulders <michielmulders1@gmail.com>
2018-01-20 23:30:26 +0100

    top sort example/test

---
+++ examples/src/topology-sorting.js
@@ -0,0 +1,80 @@
+const driver = require('bigchaindb-driver') // eslint-disable-line import/no-unresolved
+require('dotenv').config()
+
+
+// ======== Preparation ======== //
+const conn = new driver.Connection('https://test.bigchaindb.com/api/v1/', {
+    app_id: process.env.BIGCHAINDB_APP_ID,
+    app_key: process.env.BIGCHAINDB_APP_KEY
+})
+
+const alice = new driver.Ed25519Keypair()
+const bob = new driver.Ed25519Keypair()
+const charles = new driver.Ed25519Keypair()
+
+const assetdata = {
+    'bicycle': {
+        'serial_number': 'abcd1234',
+        'manufacturer': 'Bicycle Inc.',
+    }
+}
+
+const metadata = { 'planet': 'earth' }
+
+
+// ======== Create Transaction Bicycle ======== //
+const txCreateAliceSimple = driver.Transaction.makeCreateTransaction(
+    assetdata,
+    metadata,
+    [
+        driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(alice.publicKey))
+    ],
+    alice.publicKey
+)
+
+const txCreateAliceSimpleSigned =
+    driver.Transaction.signTransaction(txCreateAliceSimple, alice.privateKey)
+
+
+// ======== Post Transaction and Fetch Result ======== //
+conn.postTransaction(txCreateAliceSimpleSigned)
+    // Check status of transaction every 0.5 seconds until fulfilled
+    .then(() => conn.pollStatusAndFetchTransaction(txCreateAliceSimpleSigned.id))
+
+
+// ======== Transfer Bicycle to Bob ======== //
+    .then(() => {
+        const txTransferBob = driver.Transaction.makeTransferTransaction(
+            [{ tx: txCreateAliceSimpleSigned, output_index: 0 }],
+            [driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(bob.publicKey))],
+            { price: '100 euro' }
+        )
+
+        // Sign transfer transaction with Alice's private key
+        const txTransferBobSigned = driver.Transaction.signTransaction(txTransferBob, alice.privateKey)
+
+        return conn.postTransaction(txTransferBobSigned)
+    })
+    .then(res => conn.pollStatusAndFetchTransaction(res.id))
+
+
+// ======== Transfer Bicycle to Charles ======== //
+    .then((txTransferBob) => {
+        const txTransferCharles = driver.Transaction.makeTransferTransaction(
+            [{ tx: txTransferBob, output_index: 0 }],
+            [driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(charles.publicKey))],
+            { price: '80 euro' }
+        )
+
+        // Sign transfer transaction with Bob's private key
+        const txTransferCharlesSigned =
+            driver.Transaction.signTransaction(txTransferCharles, bob.privateKey)
+
+        return conn.postTransaction(txTransferCharlesSigned)
+    })
+    .then(res => conn.pollStatusAndFetchTransaction(res.id))
+
+
+// ======== Use Topology Sort to sort transactions for bicycle asset ======== //
+    .then(() => driver.topsortTransactions(txCreateAliceSimpleSigned.id))
+    .then((topsortTxs => console.log(topsortTxs))) // eslint-disable-line no-console
